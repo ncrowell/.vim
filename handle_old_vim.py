c5beee075e5625f3c7cb93b76966b5daaa11f238
Nicolas Crowell <nicolas.crowell@gmail.com>
2014-01-30 14:56:30 -0500

    we can't handle exceptions anyway, so simplify

--- handle_old_vim.py
+++ handle_old_vim.py
@@ -1,21 +1,12 @@
-import sys
-
-import subprocess
-import re
+from subprocess import call, check_output, CalledProcessError
+from re import findall
 
 def main():
-    try:
-        vim_version = subprocess.check_output(['vim', '--version']).split('\n')[0]
-    except subprocess.CalledProcessError as e:
-        raise Exception("Where is vim!?!", e)
-    versions = re.findall('proved ([0-9])+\.([0-9]+)', vim_version)[0]
-    try:
-        if versions[0] < 7 or versions[1] < 4:
-            subprocess.call(['make', '-f', 'Makefile.old'])
-    except IndexError as e:
-        print versions
-        sys.exit(1)
-    subprocess.call(['touch', '.check_vim_version'])
+    vim_version = check_output(['vim', '--version']).split('\n')[0]
+    versions = findall('proved ([0-9])+\.([0-9]+)', vim_version)[0]
+    if versions[0] < 7 or versions[1] < 4:
+        call(['make', '-f', 'Makefile.old'])
+    call(['touch', '.check_vim_version'])
 
 if __name__ == '__main__':
     main()
